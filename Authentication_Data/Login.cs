05f265a380e324518141fdc7a27b024b0d8768bf
talaalhussainn8n <talaal.hussain.n8n@gmail.com>
2026-06-20 17:31:58 +0200

    Erstellen von Authentication App mit Login Klasse (#1)
    * Initial commit
    
    * Ersrellen von WinForms
    
    * Erstellen von Login Klasse
    
    ---------
    
    Co-authored-by: Talaal Hussain <thussain@gemdatnoe.at>

---
+++ Authentication_Data/Login.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authentication_Data;
+
+public class Login
+{
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Der Vorname muss zwischen 3 und 100 Zeichen lang sein.")]
+    public required string Vorname { get; set; }
+
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Der Name muss zwischen 3 und 100 Zeichen lang sein.")]
+    public required string Name { get; set; }
+
+    [EmailAddress(ErrorMessage = "Bitte geben Sie eine gültige E-Mail-Adresse ein.")]
+    public required string Email { get; set; }
+
+    public DateTime Geburtsdatum { get; set; }
+
+    public int Postleitzahl { get; set; }
+
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Das Passwort muss zwischen 6 und 100 Zeichen lang sein.")]
+    public required string Ort { get; set; }
+}
---
+++ Authentication_UI/Authentication-App.cs
@@ -0,0 +1,9 @@
+namespace Authentication_UI;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
