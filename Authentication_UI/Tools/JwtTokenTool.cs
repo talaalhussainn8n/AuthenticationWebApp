617948d9d5aeb98ba3e48a854ffa49af33008c25
talaalhussainn8n <talaal.hussain.n8n@gmail.com>
2026-07-19 17:50:11 +0200

    Fix in JwtTokenTool

--- Authentication_UI/Tools/JwtTokenTool.cs
+++ Authentication_UI/Tools/JwtTokenTool.cs
@@ -35,9 +35,8 @@ public class JwtTokenTool
 
             return jwtToken;
         }
-        catch (ArgumentException e)
+        catch (ArgumentException)
         {
-            Console.WriteLine(e);
             throw;
         }
     }
